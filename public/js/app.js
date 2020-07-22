8c4618b3269c57a5f321d849c298a9dcab62bf6b
Neoxs <m.abdelkaderkharoubi@esi-sba.dz>
2020-07-22 01:36:37 +0100

    Build the header of the main app

--- public/js/app.js
+++ public/js/app.js
@@ -37334,15 +37334,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/main.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/main.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/main.scss ***!
+  \****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /home/neox/Desktop/KeyGen/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/neox/Desktop/KeyGen/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/neox/Desktop/KeyGen/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /home/neox/Desktop/KeyGen/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
